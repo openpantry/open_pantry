b09a76c47b4ca590720bfe9981d00dcea9862273
Brian Glusman <brian@stellaservice.com>
2017-02-06 21:44:50 -0500

    Hmm, credits as maps are messing with ExAdmin…
    Can definitely handle but will take more work/hide
    the field from the model for now.  Commented stub
    in Admin/User was first pass at handling…

--- web/admin/user.ex
+++ web/admin/user.ex
@@ -3,5 +3,16 @@ defmodule OpenPantry.ExAdmin.User do
 
   register_resource OpenPantry.User do
 
+    # controller do
+    #   before_filter :decode, only: [:update, :create, :edit, :new]
+
+    #   def decode(conn, params) do
+    #     if get_in params, [:user, :credits] do
+    #       params = update_in params, [:user, :credits], &(Poison.decode!(&1))
+    #     end
+    #     {conn, params}
+    #   end
+    # end
+
   end
 end
