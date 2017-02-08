fcb5ab5394c5f44dda7ac5ce531e976466b03f65
Brian Glusman <brian@stellaservice.com>
2017-02-08 09:28:54 -0500

    Minimal working credits on user, lost language and facility

--- web/admin/user.ex
+++ web/admin/user.ex
@@ -1,18 +1,55 @@
 defmodule OpenPantry.ExAdmin.User do
   use ExAdmin.Register
+  alias OpenPantry.Repo
+  alias OpenPantry.FoodGroup
 
+  import OpenPantry.Repo
+  import OpenPantry.FoodGroup
   register_resource OpenPantry.User do
 
-    # controller do
-    #   before_filter :decode, only: [:update, :create, :edit, :new]
+    index do
+      selectable_column
+      column :id
+      column :name
+      column :email
+      column :address1
+      column :address2
+      column :region
+      column :postal_code
+      column :primary_language
+      column :credits
+    end
+    show user do
+      attributes_table except: [:credits] #  all: true
 
-    #   def decode(conn, params) do
-    #     if get_in params, [:user, :credits] do
-    #       params = update_in params, [:user, :credits], &(Poison.decode!(&1))
-    #     end
-    #     {conn, params}
-    #   end
-    # end
+      panel "Credits" do
+        table_for user.credits do
+          Repo.all(FoodGroup)
+          |> Enum.each(fn(food_group) ->
+            column food_group.name |> String.to_atom
+          end)
+        end
+      end
 
+    end
+    form user do
+      inputs "User Details" do
+        input user, :name
+        input user, :email
+        input user, :address1
+        input user, :address1
+        input user, :address2
+        input user, :city
+        input user, :region
+        input user, :postal_code
+        # input user, :primary_language, OpenPantry.Language
+        # input user, :facility, OpenPantry.facility
+      end
+      inputs "Credits" do
+        input user, :credits, schema: Repo.all(FoodGroup) |> Enum.map(&({&1.name |> String.to_atom, :integer}))
+      end
+
+
+    end
   end
 end
