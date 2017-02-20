16fe9d69f88491399ffb6b14145a72fde1a02dc0
Brian Glusman <brian@stellaservice.com>
2017-02-20 16:32:00 -0500

    Initial local js behavior

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,35 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+$(function(){
+  const getAvailable = (row) => parseInt(row.find('.available').html())
+  const getQuantity = (row) => parseInt(row.find('.current-quantity').html())
+  const setQuantity = (row, newQuantity) => row.find('.current-quantity').html(newQuantity)
+
+
+  var fn, handlers = {
+    "js-add-stock": function(row){
+      if (getAvailable(row) > 0) {
+        setQuantity(row, getQuantity(row) + 1)
+      }
+    },
+    "js-remove-stock": function(row){
+      const currentQuantity = getQuantity(row)
+      if (currentQuantity > 0) {
+        setQuantity(row, currentQuantity - 1)
+      }
+    },
+    "js-clear-stock": function(row){
+      setQuantity(row, 0)
+    }
+  }
+
+
+  $('.js-stock-row').on('click', function(el){
+    if (fn = handlers[el.target.className]){
+      fn($(el.currentTarget));
+    };
+  })
+
+})
