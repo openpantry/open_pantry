08376201d767f1ba7096fc9c45cf19044e57aa27
Brian Glusman <brian@stellaservice.com>
2017-02-28 16:37:00 -0500

    Add iex config file
    Also rename test to exes

---
+++ .iex.exs
@@ -0,0 +1,12 @@
+alias OpenPantry.Repo
+alias OpenPantry.Facility
+alias OpenPantry.User
+alias OpenPantry.Stock
+alias OpenPantry.CreditType
+alias OpenPantry.UserCredit
+alias OpenPantry.Food
+alias OpenPantry.FoodGroup
+alias OpenPantry.Meal
+
+
+
