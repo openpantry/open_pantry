eb16f826d6e7c5c37bf67b1e43ba75400db7942e
Brian Glusman <brian@stellaservice.com>
2017-03-01 23:35:58 -0500

    Extract stock type to partial

--- web/views/food_selection_view.ex
+++ web/views/food_selection_view.ex
@@ -1,32 +1,9 @@
 defmodule OpenPantry.FoodSelectionView do
   use OpenPantry.Web, :view
-  alias OpenPantry.Stock
-  require IEx
+  import OpenPantry.DisplayLogic
 
   def food_by_group(credit_food_groups) do
     Enum.reduce(credit_food_groups, %{}, &(Map.put(&2, &1.name, &1.foods)))
-  end
-
-  def display_stock_type(stock_struct) do
-    stock_struct
-    |> to_string
-    |> String.split(".")
-    |> List.last
-  end
-
-  def active(first_type, [{first_type, _, _}|_tail]), do: "active"
-  def active(_, [{_, _, _}|_tail]), do: ""
-
-  def quantity(stock, package) do
-    if sd = Enum.find(package.stock_distributions, &(&1.stock_id == stock.id)) do
-      sd.quantity
-    else
-      0
-    end
-  end
-
-  def description(stock) do
-    Stock.stock_description(stock)
   end
 
 end
--- web/views/shared_view.ex
+++ web/views/shared_view.ex
@@ -1,4 +1,6 @@
 defmodule OpenPantry.SharedView do
   use OpenPantry.Web, :view
   import OpenPantry.Stock
+  import OpenPantry.DisplayLogic
+
 end
