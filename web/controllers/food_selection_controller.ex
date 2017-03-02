8ffe9fbb1c12fd06accd2890d24f170f8f5c0952
Brian Glusman <brian@stellaservice.com>
2017-03-02 10:01:43 -0500

    Don’t send down empty distributions

--- web/controllers/food_selection_controller.ex
+++ web/controllers/food_selection_controller.ex
@@ -8,7 +8,9 @@ defmodule OpenPantry.FoodSelectionController do
     user = conn.assigns.user |> Repo.preload(:facility)
     stock_by_type = Facility.stock_by_type(user.facility)
     package = UserOrder.find_or_create(user) |> Repo.preload(:stock_distributions)
-    distributions = package.stock_distributions |> Repo.preload(:stock)
+    distributions = package.stock_distributions
+                    |> Repo.preload(:stock)
+                    |> Enum.reject(&(&1.quantity == 0))
     render conn, "index.html",  stock_by_type: stock_by_type,
                                 credits: User.credits(user),
                                 package: package,
