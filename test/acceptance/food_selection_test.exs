b6279e7ebea5c89d2d65c7ac667803bfa18db582
Brian Glusman <brian@stellaservice.com>
2017-03-04 16:19:44 -0500

    Make plurals plural

--- test/acceptance/food_selection_test.exs
+++ test/acceptance/food_selection_test.exs
@@ -5,18 +5,23 @@ defmodule OpenPantry.FoodSelectionTest do
   def complete_facility do
     facility = insert(:facility)
     user = insert(:user, facility: facility)
-    food_group = insert(:food_group)
-    food = insert(:food, food_group: food_group)
-    stock = insert(:stock, facility: facility, food: food)
-    credit_type = insert(:credit_type, food_groups: [food_group])
-    insert(:user_credit, credit_type: credit_type, user: user )
-    %{credit_type: credit_type, facility: facility, user: user, stock: stock, food: food, food_group: food_group}
+    food_group1 = insert(:food_group)
+    food1 = insert(:food, food_group: food_group1)
+    food_group2 = insert(:food_group)
+    food2 = insert(:food, food_group: food_group2)
+    stock1 = insert(:stock, facility: facility, food: food1)
+    stock2 = insert(:stock, facility: facility, food: food2)
+    credit_type1 = insert(:credit_type, food_groups: [food_group1])
+    credit_type2 = insert(:credit_type, food_groups: [food_group2])
+    insert(:user_credit, credit_type: credit_type1, user: user )
+    insert(:user_credit, credit_type: credit_type2, user: user )
+    %{credit_types: [credit_type1, credit_type2], facility: facility, user: user, stocks: [stock1, stock2], foods: [food1, food2], food_groups: [food_group1, food_group2]}
   end
 
 
   @tag timeout: Ownership.timeout
   test "selection table has tab per credit type, plus meals and cart", %{session: session} do
-    %{credit_type: credit_type} = complete_facility()
+    %{credit_types: [credit_type|_]} = complete_facility()
 
     first_credit = session
     |> visit("/en/food_selections")
