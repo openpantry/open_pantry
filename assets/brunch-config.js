f63e15f82c3699b4cb8ff15cddf285485f73aeb2
Brian Glusman <brian@stellaservice.com>
2017-03-04 10:24:09 -0500

    Reference default brunch generated by 1.3

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -2,37 +2,26 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: {
-        "js/app.js": ["js\/app.js", "js\/facility.js", "js\/socket.js"],
-        "js/node_deps.js": /^(node_modules)/,
-        "js/ex_admin_common.js": ["vendor/ex_admin_common.js"],
-        "js/admin_lte2.js": ["vendor/admin_lte2.js"],
-        "js/jquery.min.js": ["vendor/jquery.min.js"]
-      }
+      joinTo: "js/app.js"
+
       // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
+      // https://github.com/brunch/brunch/blob/master/docs/config.md#files
       // joinTo: {
-      //  "js/app.js": /^(assets\/js)/,
-      //  "js/vendor.js": /^(assets\/vendor)|(deps)/
+      //  "js/app.js": /^(js)/,
+      //  "js/vendor.js": /^(vendor)|(deps)/
       // }
       //
       // To change the order of concatenation of files, explicitly mention here
+      // https://github.com/brunch/brunch/tree/master/docs#concatenation
       // order: {
       //   before: [
-      //     "assets/vendor/js/jquery-2.1.1.js",
-      //     "assets/vendor/js/bootstrap.min.js"
+      //     "vendor/js/jquery-2.1.1.js",
+      //     "vendor/js/bootstrap.min.js"
       //   ]
       // }
     },
     stylesheets: {
-      joinTo: {
-        "css/app.css": /^(css)/,
-        "css/admin_lte2.css": ["vendor/admin_lte2.css"],
-        "css/active_admin.css.css": ["vendor/active_admin.css.css"],
-      },
-      order: {
-        after: ["assets/css/app.css"] // concat app.css last
-      }
+      joinTo: "css/app.css"
     },
     templates: {
       joinTo: "js/app.js"
@@ -49,8 +38,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-     watched: ["static", "css", "js", "vendor"],
-
+    watched: ["static", "css", "js", "vendor"],
     // Where to compile files to
     public: "../priv/static"
   },
@@ -65,11 +53,7 @@ exports.config = {
 
   modules: {
     autoRequire: {
-      "js/app.js": ["js/app"],
-      "js/node_deps.js": ["js/node_deps.js"],
-      "js/ex_admin_common.js":["js/ex_admin_common.js"],
-      "js/admin_lte2.js":["js/admin_lte2.js"],
-      "js/jquery.min.js":["js/jquery.min.js"]
+      "js/app.js": ["js/app"]
     }
   },
 
