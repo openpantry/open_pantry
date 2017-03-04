74a6feb84a797059624c90e5e7be3727bc3059f8
Brian Glusman <brian@stellaservice.com>
2017-03-04 15:45:33 -0500

    Extract setup to function

--- test/acceptance/food_selection_test.exs
+++ test/acceptance/food_selection_test.exs
@@ -2,15 +2,21 @@ defmodule OpenPantry.FoodSelectionTest do
   use OpenPantry.AcceptanceCase, async: true
   import OpenPantry.Factory
 
-  @tag timeout: Ownership.timeout
-  test "selection table has tab per credit type, plus meals and cart", %{session: session} do
+  def complete_facility do
     facility = insert(:facility)
     user = insert(:user, facility: facility)
     food_group = insert(:food_group)
     food = insert(:food, food_group: food_group)
-    insert(:stock, facility: facility, food: food)
+    stock = insert(:stock, facility: facility, food: food)
     credit_type = insert(:credit_type, food_groups: [food_group])
     insert(:user_credit, credit_type: credit_type, user: user )
+    %{credit_type: credit_type, facility: facility, user: user, stock: stock, food: food, food_group: food_group}
+  end
+
+
+  @tag timeout: Ownership.timeout
+  test "selection table has tab per credit type, plus meals and cart", %{session: session} do
+    %{credit_type: credit_type} = complete_facility()
 
     first_credit = session
     |> visit("/en/food_selections")
