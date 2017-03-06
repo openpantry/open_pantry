85a65acbeccc936b6149349df2da18c877bb199d
Brian Glusman <brian@stellaservice.com>
2017-03-06 00:08:39 -0500

    Have wallaby save screenshots to Circle artifacts dir

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,7 +2,10 @@ ExUnit.configure(exclude: [pending: true])
 ExUnit.start
 
 Ecto.Adapters.SQL.Sandbox.mode(OpenPantry.Repo, :manual)
-{:ok, _} = Application.ensure_all_started(:wallaby)
-{:ok, _} = Application.ensure_all_started(:ex_machina)
 Application.put_env(:wallaby, :base_url, OpenPantry.Endpoint.url)
 Application.put_env(:wallaby, :screenshot_on_failure, true)
+if System.get_env("CIRCLE_ARTIFACTS") do
+  Application.put_env(:wallaby, :screenshot_dir, System.get_env("CIRCLE_ARTIFACTS"))
+end
+{:ok, _} = Application.ensure_all_started(:wallaby)
+{:ok, _} = Application.ensure_all_started(:ex_machina)
