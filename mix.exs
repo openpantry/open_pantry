ce5e09c203f006f94a52bf4bb7da0d903bc62dd9
Brian Glusman <brian@stellaservice.com>
2017-04-04 12:30:09 -0400

    Set mix to use correct elixir version?  Because, it should?  Why not an issue before? (#69)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OpenPantry.Mixfile do
   def project do
     [app: :open_pantry,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
