3183118c6bb619bc6063b6cf9cf121a7e22cab16
Brian Glusman <brian@stellaservice.com>
2017-05-18 16:12:36 -0400

    Add additional fields to manage/orders and minor refactors

--- lib/open_pantry/web/views/food_selection_view.ex
+++ lib/open_pantry/web/views/food_selection_view.ex
@@ -1,6 +1,7 @@
 defmodule OpenPantry.Web.FoodSelectionView do
   use OpenPantry.Web, :view
   import OpenPantry.Web.DisplayLogic
+  alias OpenPantry.Web.SharedView
 
   def food_by_group(credit_food_groups) do
     Enum.reduce(credit_food_groups, %{}, &(Map.put(&2, &1.name, &1.foods)))
--- lib/open_pantry/web/views/user_order_view.ex
+++ lib/open_pantry/web/views/user_order_view.ex
@@ -1,4 +1,8 @@
 defmodule OpenPantry.Web.UserOrderView do
   use OpenPantry.Web, :view
+  alias OpenPantry.Web.SharedView
 
+  def image_url(stock) do
+    SharedView.image_url(stock)
+  end
 end
