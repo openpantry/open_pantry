6998006631496f34d90e8a227ed25f69c0a82481
Brian Glusman <brian@stellaservice.com>
2017-05-23 16:28:37 -0400

    Display add-to-cart OR quantity-control not both

--- lib/open_pantry/web/views/display_logic.ex
+++ lib/open_pantry/web/views/display_logic.ex
@@ -1,5 +1,5 @@
 defmodule OpenPantry.Web.DisplayLogic do
-
+  @hidden_class "hidden"
   def active(first_type, [{first_type, _, _}|_tail]), do: "active"
   def active(_, [{_, _, _}|_tail]), do: ""
 
@@ -16,6 +16,18 @@ defmodule OpenPantry.Web.DisplayLogic do
     |> to_string
     |> String.split(".")
     |> List.last
+  end
+
+  def hidden_quantity(stock, package) do
+    if quantity(stock, package) == 0 do
+      @hidden_class
+    end
+  end
+
+  def hidden_add_to_cart(stock, package) do
+    if quantity(stock, package) != 0 do
+      @hidden_class
+    end
   end
 
 end
