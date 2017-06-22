e66ec2be135abc21ad185c444a2e2e7187ee1672
Brian Glusman <brian@stellaservice.com>
2017-06-21 22:20:11 -0400

    Move max limits to stock from offer
    Also actually validate they aren’t exceeded, 
    but only when quantity is increasing, so can still
    remove units when at limit

--- lib/open_pantry/web/models/stock_distribution.ex
+++ lib/open_pantry/web/models/stock_distribution.ex
@@ -7,6 +7,7 @@ defmodule OpenPantry.StockDistribution do
     field :quantity, :integer
     belongs_to :stock, Stock
     belongs_to :user_order, UserOrder
+    has_one :user, through: [:user_order, :user]
 
     timestamps()
   end
@@ -18,8 +19,40 @@ defmodule OpenPantry.StockDistribution do
     struct
     |> cast(params, [:quantity, :stock_id, :user_order_id])
     |> validate_required([:quantity, :stock_id, :user_order_id])
+    |> validate_max_per_order
     |> unique_constraint(:unique_stock_per_package, name: :unique_stock_per_package)
     |> check_constraint(:quantity, name: :non_negative_quantity)
+  end
+
+  def validate_max_per_order(changeset, options \\ []) do
+    validate_change(changeset, :quantity, fn _, quantity ->
+
+      case validate_stock_distribution(%{}, changeset.data) do
+        {:ok, _} ->
+          []
+        {:error, _} ->
+          [{:quantity, options[:message] || "Over max allowed quantity per person or per order"}]
+      end
+    end)
+  end
+
+  def validate_stock_distribution(map, stock_distribution, quantity \\ 1) do
+    if quantity < 1 || per_person_ok?(stock_distribution) && per_package_ok?(stock_distribution) do
+      {:ok, map}
+    else
+      {:error, map}
+    end
+  end
+
+  defp per_person_ok?(stock_distribution = %StockDistribution{ stock: %Stock{ max_per_person: nil } }), do: true
+  defp per_package_ok?(stock_distribution = %StockDistribution{ stock: %Stock{ max_per_package: nil } }), do: true
+
+  defp per_person_ok?(stock_distribution) do
+    stock_distribution.quantity <= stock_distribution.stock.max_per_person * stock_distribution.user.family_members
+  end
+
+  defp per_package_ok?(stock_distribution) do
+    stock_distribution.quantity <= stock_distribution.stock.max_per_package
   end
 
   @spec package(integer()) :: UserOrder.t
@@ -29,22 +62,29 @@ defmodule OpenPantry.StockDistribution do
 
   @spec find_or_create(integer(), integer()) :: StockDistribution.t
   def find_or_create(user_order_id, stock_id) do
-    find_by_package_and_stock(user_order_id, stock_id) ||
-    %StockDistribution{ user_order_id: user_order_id,
-                        stock_id: stock_id, quantity: 0}
-    |> Repo.insert!()
+    exisiting_distribution = find_by_package_and_stock(user_order_id, stock_id, [:user, :stock])
+    if exisiting_distribution do
+      exisiting_distribution
+    else
+      stock = Stock.find(stock_id)
+      user = UserOrder.find(user_order_id, [:user]).user
+      %StockDistribution{ user_order_id: user_order_id,
+                          stock_id: stock_id, quantity: 0, user: user, stock: stock}
+      |> Repo.insert!()
+    end
   end
 
   @spec query_by_package_and_stock(integer(), integer()) :: Ecto.Query.t
-  def query_by_package_and_stock(package_id, stock_id) do
+  def query_by_package_and_stock(package_id, stock_id, preload \\ []) do
     from(sd in StockDistribution,
     where: sd.user_order_id == ^package_id,
-    where: sd.stock_id == ^stock_id)
+    where: sd.stock_id == ^stock_id,
+    preload: ^preload)
   end
 
   @spec find_by_package_and_stock(integer(), integer()) :: StockDistribution.t
-  def find_by_package_and_stock(package_id, stock_id) do
-    query_by_package_and_stock(package_id, stock_id)
+  def find_by_package_and_stock(package_id, stock_id, preload \\ []) do
+    query_by_package_and_stock(package_id, stock_id, preload)
     |> Repo.one
   end
 
