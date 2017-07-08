a05b12e8f0110bbd74c59250d67ad382f9d9f44b
Brian Glusman <brian@stellaservice.com>
2017-07-08 17:26:18 -0400

    1 day validity rather than 3

--- lib/open_pantry/web/views/user_selection_view.ex
+++ lib/open_pantry/web/views/user_selection_view.ex
@@ -3,7 +3,7 @@ defmodule OpenPantry.Web.UserSelectionView do
 
   def login_token(user) do
     expiry  =  Timex.now
-            |> Timex.add( Timex.Duration.from_days(3))
+            |> Timex.add( Timex.Duration.from_days(1))
             |> Timex.to_unix
     {:ok, token, _claim} = Guardian.encode_and_sign(user, :access, %{exp: expiry })
     token
