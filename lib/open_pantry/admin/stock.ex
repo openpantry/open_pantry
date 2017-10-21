c10e20f65264fafe8e2c867e131897a06f2e8d2a
Frédérick Capovilla <caposite@gmail.com>
2017-10-21 12:45:14 -0400

    Add a custom scope sorted by food name to the Stock index page.

--- lib/open_pantry/admin/stock.ex
+++ lib/open_pantry/admin/stock.ex
@@ -22,9 +22,20 @@ defmodule OpenPantry.ExAdmin.Stock do
         input stock, :food, collection: OpenPantry.Food.all
         input stock, :facility, collection: Facility.all
       end
-
-
     end
+
+    scope :sorted_by_food, [default: true], fn(query) ->
+      case query do
+        %Ecto.Query{} ->
+          query
+          |> exclude(:order_by)
+          |> join(:left, [s], f in assoc(s, :food))
+          |> order_by([s, f], asc: f.longdesc)
+        _ ->
+          query
+      end
+    end
+    scope :all, default: false
   end
 
   def display_name(stock) do
