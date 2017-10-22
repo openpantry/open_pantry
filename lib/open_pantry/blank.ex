b4eb1f4065f5864287fbaecc3a2d21ba5783ec7e
Dave Aronson <davearonson@users.noreply.github.com>
2017-10-22 14:17:01 -0400

    add doctests on Blank

--- lib/open_pantry/blank.ex
+++ lib/open_pantry/blank.ex
@@ -5,6 +5,55 @@ defmodule Blank do
 
   @doc """
   Returns `true` if data is considered blank/empty.
+
+  ## Examples
+
+    iex> Blank.blank? ""  # empty is the only blank string
+    true
+
+    iex> Blank.blank? " "  # not even whitespace-only is blank
+    false
+
+    iex> Blank.blank? "hello, world!"
+    false
+
+    iex> Blank.blank? 0  # integers are never blank, not even 0
+    false
+
+    iex> Blank.blank? 1
+    false
+
+    # (other types of numbers have not been implemented yet)
+
+    iex> Blank.blank? []  # empty is the only blank list
+    true
+
+    iex> Blank.blank? [:foo]
+    false
+
+    iex> Blank.blank? [[]]  # having only blank elements doesn't make a list blank
+    false
+
+    iex> Blank.blank? %{}  # empty is the only blank map
+    true
+
+    iex> Blank.blank? %{ foo: :bar }
+    false
+
+    iex> Blank.blank? %{ "" => "" }  # blank keys/values don't make a map blank
+    false
+
+    iex> Blank.blank? false  # false and nil are the only blank atoms
+    true
+
+    iex> Blank.blank? nil
+    true
+
+    iex> Blank.blank? true
+    false
+
+    iex> Blank.blank? :any_other_atom
+    false
   """
   def blank?(data) do
     Blank.Protocol.blank?(data)
@@ -12,13 +61,71 @@ defmodule Blank do
 
   @doc """
   Returns `true` if data is not considered blank/empty.
+  
+  ## Examples
+
+    iex> Blank.present? ""  # empty is the only blank string
+    false
+
+    iex> Blank.present? " "  # not even whitespace-only is blank
+    true
+
+    iex> Blank.present? "hello, world!"
+    true
+
+    iex> Blank.present? 0  # integers are never blank, not even 0
+    true
+
+    iex> Blank.present? 1
+    true
+
+    # (other types of numbers have not been implemented yet)
+
+    iex> Blank.present? []  # empty is the only blank list
+    false
+
+    iex> Blank.present? [:foo]
+    true
+
+    iex> Blank.present? [[]]  # having only blank elements doesn't make a list blank
+    true
+
+    iex> Blank.present? %{}  # empty is the only blank map
+    false
+
+    iex> Blank.present? %{ foo: :bar }
+    true
+
+    iex> Blank.present? %{ "" => "" }  # blank keys/values don't make a map blank
+    true
+
+    iex> Blank.present? false  # false and nil are the only blank atoms
+    false
+
+    iex> Blank.present? nil
+    false
+
+    iex> Blank.present? true
+    true
+
+    iex> Blank.present? :any_other_atom
+    true
   """
   def present?(data) do
     !blank?(data)
   end
 
   @doc """
-  Returns the provided `data` if valid of the `default` value if not.
+  Returns the provided `data` if present (according to `Blank.present?`),
+  else the `default` value.
+
+  ## Examples
+
+    iex> Blank.default_to("", 42)
+    42
+
+    iex> Blank.default_to("hello, world!", 42)
+    "hello, world!"
   """
   def default_to(data, default) do
     if blank?(data), do: default, else: data
