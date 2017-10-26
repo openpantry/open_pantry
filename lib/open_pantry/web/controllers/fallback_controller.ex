baa067717f488c37b7f45e11dbd6b67b8801b57c
Nathaniel Waisbrot <nathaniel@waisbrot.net>
2017-10-25 22:28:06 -0400

    fix call to Phoenix.Controller.render/4
    This looks like it's meant to be a call to render/4 but with only 3 args
    it becomes the (very different) render/3.

--- lib/open_pantry/web/controllers/fallback_controller.ex
+++ lib/open_pantry/web/controllers/fallback_controller.ex
@@ -15,6 +15,6 @@ defmodule OpenPantry.Web.FallbackController do
   def call(conn, {:error, :not_found}) do
     conn
     |> put_status(:not_found)
-    |> render(OpenPantry.Web.ErrorView, :"404")
+    |> render(OpenPantry.Web.ErrorView, :"404", [])
   end
 end
