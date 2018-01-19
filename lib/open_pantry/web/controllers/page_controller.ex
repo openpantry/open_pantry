0ad8f6efe73178ba0f5891d5397322865f21116f
Brian Underwood <brian@brian-underwood.codes>
2018-01-19 16:59:54 -0500

    Semantic UI and getting things in a mostly reasonable order

--- lib/open_pantry/web/controllers/page_controller.ex
+++ lib/open_pantry/web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule OpenPantry.Web.PageController do
   use OpenPantry.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html", languages: OpenPantry.Language.primary_list
+    render conn, "index.html", languages: OpenPantry.Language.primary_list, landing_page: true
   end
 end
